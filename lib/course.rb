ec03ef779a2f35946c648fc536d0a7108435d934
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-26 04:04:31 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
-
-
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
+  def initialize
+    @@all << self 
+  end
+  def self.all 
+    @@all 
+  end 
+  def self.reset_all 
+    @@all.clear 
+  end
+#   def initialize(title, schedule, description)
+#     @title = "Programming Robots for Outer Space"
+#     @schedule = "Full-Time"
+#     @description = "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover" 
+# end
+end
